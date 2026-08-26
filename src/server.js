ec4c6d4fa27cdc3df0577d9a8f037ef98fea535b
LasSs_666 <lassslasss02@gmail.com>
2026-08-26 19:02:38 +0300

    Fix: rename middleware folder and add full content for evaluation

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectMongoDB } from './db/connectMongoDB.js';
-import { logger } from './middlewares/logger.js';
-import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import { errorHandler } from './middlewares/errorHandler.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
 import notesRouter from './routes/notesRoutes.js';
 
 dotenv.config();
