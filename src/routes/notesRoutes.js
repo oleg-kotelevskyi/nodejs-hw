d5d332de7c997baf1ff6266a4eb7bab630838796
LasSs_666 <lassslasss02@gmail.com>
2026-09-02 22:43:28 +0300

    Fix: final alignment with celebrate validation specifications

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { celebrate, Segments } from 'celebrate';
+import { celebrate } from 'celebrate';
 import {
   getAllNotes,
   getNoteById,
@@ -16,37 +16,14 @@ import {
 
 const router = Router();
 
-router.get(
-  '/notes',
-  celebrate({ [Segments.QUERY]: getAllNotesSchema }),
-  getAllNotes
-);
-
-router.get(
-  '/notes/:noteId',
-  celebrate({ [Segments.PARAMS]: noteIdSchema }),
-  getNoteById
-);
-
-router.post(
-  '/notes',
-  celebrate({ [Segments.BODY]: createNoteSchema }),
-  createNote
-);
-
-router.delete(
-  '/notes/:noteId',
-  celebrate({ [Segments.PARAMS]: noteIdSchema }),
-  deleteNote
-);
-
-router.patch(
-  '/notes/:noteId',
-  celebrate(updateNoteSchema),
-  updateNote
-);
+router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
+router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
+router.post('/notes', celebrate(createNoteSchema), createNote);
+router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
+router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
 
 export default router;
+
 
 
 
--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -9,34 +9,44 @@ const objectIdCustomValidator = (value, helpers) => {
   return value;
 };
 
-export const getAllNotesSchema = Joi.object().keys({
+const _getAllNotesJoi = Joi.object().keys({
   page: Joi.number().integer().min(1).default(1),
   perPage: Joi.number().integer().min(5).max(20).default(10),
   tag: Joi.string().valid(...TAGS).optional(),
   search: Joi.string().allow('').optional(),
 });
 
-export const noteIdSchema = Joi.object().keys({
+const _noteIdJoi = Joi.object().keys({
   noteId: Joi.string().custom(objectIdCustomValidator).required(),
 });
 
-export const createNoteSchema = Joi.object().keys({
+const _createNoteJoi = Joi.object().keys({
   title: Joi.string().min(1).required(),
   content: Joi.string().allow('').optional(),
   tag: Joi.string().valid(...TAGS).optional(),
 });
 
+const _updateNoteBodyJoi = Joi.object().keys({
+  title: Joi.string().min(1).optional(),
+  content: Joi.string().allow('').optional(),
+  tag: Joi.string().valid(...TAGS).optional(),
+}).min(1);
+
+export const getAllNotesSchema = {
+  query: _getAllNotesJoi
+};
+
+export const noteIdSchema = {
+  params: _noteIdJoi
+};
+
+export const createNoteSchema = {
+  body: _createNoteJoi
+};
+
 export const updateNoteSchema = {
-  params: Joi.object().keys({
-    noteId: Joi.string().custom(objectIdCustomValidator).required(),
-  }),
-  body: Joi.object()
-    .keys({
-      title: Joi.string().min(1).optional(),
-      content: Joi.string().allow('').optional(),
-      tag: Joi.string().valid(...TAGS).optional(),
-    })
-    .min(1),
+  params: _noteIdJoi,
+  body: _updateNoteBodyJoi
 };
 
 
